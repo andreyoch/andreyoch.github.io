a0d67f0fd539de52d0f1dab7bb0ddf56af8c4803
Andrew T <at100991@student.sgh.waw.pl>
2021-01-07 17:12:14 +0200

    add prototype winner check

--- js/script.js
+++ js/script.js
@@ -7,20 +7,59 @@ const game = (() => {
         ${_board[1]}
         ${_board[2]}`)
     }
+
     function showBoardArray() {
         console.log(_board);
     }
-    function receiveTurn(line, cell, side,playerNameWhoMakeTurn) {
+
+    function checkWinner() {
+        //If first line equal
+        if ((_board[0][0] === _board[0][1]) && _board[0][2] === _board[0][0]) {
+            game.showWinner(_board[0][0]);
+            //If first column equal
+        } else if ((_board[0][0] === _board[1][0]) && _board[2][0] === _board[0][0]) {
+            game.showWinner(_board[0][0]);
+            //If second column equal
+        } else if ((_board[0][1] === _board[1][1]) && _board[2][1] === _board[0][1]) {
+            game.showWinner(_board[0][1]);
+            //If third column equal
+        } else if ((_board[0][2] === _board[1][2]) && _board[2][2] === _board[0][2]) {
+            game.showWinner(_board[0][2])
+            //If second line equal
+        } else if ((_board[1][0] === _board[1][1]) && _board[1][2] === _board[1][0]) {
+            game.showWinner(_board[1][0]);
+            //If third line equal
+        } else if ((_board[2][0] === _board[2][1]) && _board[2][2] === _board[2][0]) {
+            game.showWinner(_board[2][0]);
+            //If equal from top to bottom
+        } else if ((_board[0][0] === _board[1][1]) && _board[2][2] === _board[0][0]) {
+            game.showWinner(_board[0][0]);
+            //If equal from bottom to top
+        } else if ((_board[0][2] === _board[1][1]) && _board[2][1] === _board[0][2]) {
+            game.showWinner(_board[0][2])
+        }
+    }
+
+    function showWinner(side) {
+        if (side === 'X') {
+            console.log(`${player1.getName()} win!`)
+        } else {
+            console.log(`${player2.getName()} win!`);
+        }
+    }
+
+    function receiveTurn(line, cell, side, playerNameWhoMakeTurn) {
         line--;
         cell--;
         let turn;
         side === 'X' ? turn = 'X' : turn = 'O';
         _board[line][cell] = turn;
         game.showBoard();
+        game.checkWinner();
         console.log(`${playerNameWhoMakeTurn} make turn`)
     }
 
-    return {showBoard, receiveTurn,showBoardArray};
+    return {showBoard, receiveTurn, showBoardArray, checkWinner, showWinner};
 
 
 })()
@@ -35,7 +74,7 @@ function player(name, side) {
     }
 
     function makeTurn(line, cell) {
-        game.receiveTurn(line, cell, side,name)
+        game.receiveTurn(line, cell, side, name)
     }
 
     return {getName, getSide, makeTurn}
@@ -44,10 +83,6 @@ function player(name, side) {
 const player1 = player('Andrew', 'X');
 const player2 = player('Computer', 'O')
 game.showBoard()
-player1.makeTurn(1, 3);
-player2.makeTurn(2, 2)
-player1.makeTurn(2, 3)
-player2.makeTurn(3, 3)
-player1.makeTurn(3,1)
-player2.makeTurn(1,1)
-game.showBoardArray()
+player2.makeTurn(1, 1)
+player2.makeTurn(1, 2)
+player2.makeTurn(1, 3)
