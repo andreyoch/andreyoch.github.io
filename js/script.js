a5ed940ca43c206a713e17c078de3800b47364a0
Andrew Tsishevskii <at100991@student.sgh.waw.pl>
2021-01-07 20:15:13 -0800

    Change check winner algorithm

--- js/script.js
+++ js/script.js
@@ -12,31 +12,31 @@ const game = (() => {
         console.log(_board);
     }
 
-    function checkWinner() {
+    function checkWinner(side) {
         //If first line equal
-        if ((_board[0][0] === _board[0][1]) && _board[0][2] === _board[0][0]) {
-            game.showWinner(_board[0][0]);
+        if (_board[0][0] === side && _board[0][1] === side && _board[0][2] === side) {
+            game.showWinner(side);
             //If first column equal
-        } else if ((_board[0][0] === _board[1][0]) && _board[2][0] === _board[0][0]) {
-            game.showWinner(_board[0][0]);
+        } else if (_board[0][0] === side && _board[1][0] === side && _board[2][0] === side) {
+            game.showWinner(side);
             //If second column equal
-        } else if ((_board[0][1] === _board[1][1]) && _board[2][1] === _board[0][1]) {
-            game.showWinner(_board[0][1]);
+        } else if (_board[0][1] === side && _board[1][1] === side && _board[2][1] === side) {
+            game.showWinner(side);
             //If third column equal
-        } else if ((_board[0][2] === _board[1][2]) && _board[2][2] === _board[0][2]) {
-            game.showWinner(_board[0][2])
+        } else if (_board[0][2] === side && _board[1][2] === side && _board[2][2] === side) {
+            game.showWinner(side)
             //If second line equal
-        } else if ((_board[1][0] === _board[1][1]) && _board[1][2] === _board[1][0]) {
-            game.showWinner(_board[1][0]);
+        } else if (_board[1][0] === side && _board[1][1] === side && _board[1][2] === side) {
+            game.showWinner(side);
             //If third line equal
-        } else if ((_board[2][0] === _board[2][1]) && _board[2][2] === _board[2][0]) {
-            game.showWinner(_board[2][0]);
+        } else if (_board[2][0] === side && _board[2][1] === side && _board[2][2] === side) {
+            game.showWinner(side);
             //If equal from top to bottom
-        } else if ((_board[0][0] === _board[1][1]) && _board[2][2] === _board[0][0]) {
-            game.showWinner(_board[0][0]);
+        } else if (_board[0][0] === side && _board[1][1] === side && _board[2][2] === side) {
+            game.showWinner(side);
             //If equal from bottom to top
-        } else if ((_board[0][2] === _board[1][1]) && _board[2][1] === _board[0][2]) {
-            game.showWinner(_board[0][2])
+        } else if (_board[0][2] === side && _board[1][1] === side && _board[2][1] === side) {
+            game.showWinner(side)
         }
     }
 
@@ -48,18 +48,19 @@ const game = (() => {
         }
     }
 
-    function receiveTurn(line, cell, side, playerNameWhoMakeTurn) {
+    function receiveTurn(line, cell, side, playerNameWhoMadeTurn) {
         line--;
         cell--;
         let turn;
         side === 'X' ? turn = 'X' : turn = 'O';
         _board[line][cell] = turn;
         game.showBoard();
-        game.checkWinner();
-        console.log(`${playerNameWhoMakeTurn} make turn`)
+        game.checkWinner(side);
+        console.log(`${playerNameWhoMadeTurn} made turn`)
     }
 
-    return {showBoard, receiveTurn, showBoardArray, checkWinner, showWinner};
+
+    return {showBoard, receiveTurn, checkWinner, showWinner,};
 
 
 })()
@@ -83,6 +84,8 @@ function player(name, side) {
 const player1 = player('Andrew', 'X');
 const player2 = player('Computer', 'O')
 game.showBoard()
-player2.makeTurn(1, 1)
+player1.makeTurn(1, 1)
 player2.makeTurn(1, 2)
+player1.makeTurn(2, 1)
 player2.makeTurn(1, 3)
+player1.makeTurn(3, 1)
