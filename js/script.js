e5dd274352084abd5a75062870ca8acf13d3ba1b
Andrew Tsishevskii <at100991@student.sgh.waw.pl>
2021-01-13 17:38:59 +0200

    Code refactor

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const game = (() => {
     function setBoard(boardArray) {
         _board = boardArray;
     }
+
     function clearBoardArray() {
         const _clearBoardArray = _board.map(array1 => array1.map(value => value = ''));
         setBoard(_clearBoardArray);
@@ -17,58 +18,33 @@ const game = (() => {
     function checkWinner(side) {
         //If first line equal
         if (_board[0][0] === side && _board[0][1] === side && _board[0][2] === side) {
-            game.showWinner(side);
             return true;
             //If first column equal
         } else if (_board[0][0] === side && _board[1][0] === side && _board[2][0] === side) {
-            game.showWinner(side);
             return true;
             //If second column equal
         } else if (_board[0][1] === side && _board[1][1] === side && _board[2][1] === side) {
-            game.showWinner(side);
             return true;
             //If third column equal
         } else if (_board[0][2] === side && _board[1][2] === side && _board[2][2] === side) {
-            game.showWinner(side)
             return true;
             //If second line equal
         } else if (_board[1][0] === side && _board[1][1] === side && _board[1][2] === side) {
-            game.showWinner(side);
             return true;
             //If third line equal
         } else if (_board[2][0] === side && _board[2][1] === side && _board[2][2] === side) {
-            game.showWinner(side);
             return true;
             //If equal from top to bottom
         } else if (_board[0][0] === side && _board[1][1] === side && _board[2][2] === side) {
-            game.showWinner(side);
             return true;
             //If equal from bottom to top
         } else if (_board[0][2] === side && _board[1][1] === side && _board[2][0] === side) {
-            game.showWinner(side);
             return true;
         }
     }
 
-    function showWinner(side) {
-        if (side === 'X') {
-            console.log(`${player1.getName()} win!`)
-        } else {
-            console.log(`${player2.getName()} win!`);
-        }
-    }
-
-    function receiveTurn(line, cell, side, playerNameWhoMadeTurn) {
-        line--;
-        cell--;
-        let turn;
-        side === 'X' ? turn = 'X' : turn = 'O';
-        _board[line][cell] = turn;
-        game.checkWinner(side);
-        console.log(`${playerNameWhoMadeTurn} made turn`)
-    }
-    const makeTurn = function listenToTurn(cell){
-        return function (e) {
+    const makeTurn = function listenToTurn(cell) {
+        return function () {
             console.log(cell);
             const userSide = player1.getSide();
             const name = player1.getName();
@@ -78,12 +54,12 @@ const game = (() => {
             board[lineNumber][cellNumber] = userSide;
             game.setBoard(board);
             const userSelectedCell = document.querySelector(`#cell-${lineNumber}${cellNumber}`);
-            if(userSide === 'X') {
+            if (userSide === 'X') {
                 userSelectedCell.style = 'background: url("../images/cross.png") no-repeat center';
             } else {
                 userSelectedCell.style = 'background: url("../images/ellipse.png") no-repeat center';
             }
-            if(game.checkWinner(userSide)) {
+            if (game.checkWinner(userSide)) {
                 endRound(name);
             } else {
                 computerTurn(userSide);
@@ -91,7 +67,7 @@ const game = (() => {
         }
 
     }
-    return {getBoard, receiveTurn, checkWinner, showWinner,setBoard,clearBoardArray,makeTurn};
+    return {getBoard, checkWinner, setBoard, clearBoardArray, makeTurn};
 
 
 })()
@@ -103,47 +79,41 @@ function player() {
     function getSide() {
         return _side;
     }
+
     function setSide(side) {
         _side = side;
     }
+
     function getName() {
         return _name;
     }
+
     function setName(name) {
         _name = name;
     }
 
-
-    function makeTurn(line, cell) {
-        game.receiveTurn(line, cell, side, name)
-    }
-
-    return {getName, makeTurn,getSide,setName,setSide}
+    return {getName, getSide, setName, setSide}
 }
 
 const player1 = player();
-const player2 = player();
 
 
-document.addEventListener('DOMContentLoaded',main);
+document.addEventListener('DOMContentLoaded', main);
 
-function main (){
+function main() {
     listenToTypeOfGame()
 }
 
-function playRound(userSide,name) {
-
-    let playerScore = 0;
-    let computerScore = 0;
+function playRound() {
     const cells = document.querySelectorAll('.cell');
     cells.forEach(cell => cell.style = 'background : ;');
-    cells.forEach(cell => cell.addEventListener('click',game.makeTurn(cell)),{once:true});
+    cells.forEach(cell => cell.addEventListener('click', game.makeTurn(cell)), {once: true});
 }
 
-function listenToTypeOfGame () {
-    const gameTypeBtns  = document.querySelectorAll('.button-row_button');
-    gameTypeBtns.forEach(btn => btn.addEventListener('click',(e) => {
-        if(btn.textContent === 'Single Player') {
+function listenToTypeOfGame() {
+    const gameTypeButtons = document.querySelectorAll('.button-row_button');
+    gameTypeButtons.forEach(btn => btn.addEventListener('click', () => {
+        if (btn.textContent === 'Single Player') {
             singlePlayer();
         } else {
             multiPlayer();
@@ -158,17 +128,17 @@ function singlePlayer() {
     const enterNameScreen = document.querySelector('.single-player-name_enter');
     enterNameScreen.style = 'display: block';
     const singlePlayerNextBtn = document.querySelector('.single-player_next-btn');
-    singlePlayerNextBtn.addEventListener('click',(e) => {
+    singlePlayerNextBtn.addEventListener('click', () => {
         const playerName = document.querySelector('.name-input').value;
         enterNameScreen.style = 'display: none';
         const pickASideScreen = document.querySelector('.pick-a-side');
         pickASideScreen.style = 'display: block';
         const playBtn = document.querySelector('.pick-a-side_btn');
-        playBtn.addEventListener('click',e => {
+        playBtn.addEventListener('click', () => {
             let side;
             //If cross selected return true,if ellipse return false
             const selectedSide = document.querySelector('.buttons-row_button').checked;
-            if(selectedSide) {
+            if (selectedSide) {
                 side = 'X';
             } else {
                 side = 'O';
@@ -178,7 +148,7 @@ function singlePlayer() {
             gameBoard.style = 'display: block';
             player1.setName(playerName);
             player1.setSide(side);
-            playRound(side,playerName);
+            playRound(side, playerName);
         })
     })
 }
@@ -187,38 +157,38 @@ function multiPlayer() {
 
 }
 
-function computerTurn (userSide) {
+function computerTurn(userSide) {
     const board = game.getBoard();
     let lineNumber;
     let cellNumber;
     let condition = true;
     let computerSide;
-    while(condition) {
+    while (condition) {
         //Generate a random number for line and cell,and check,if cell not empty-repeat
-         lineNumber = Math.floor((Math.random() * (3)));
-         cellNumber = Math.floor((Math.random() * (3)));
-         if(!board[lineNumber][cellNumber]){
-             //Update value in array
-             if(userSide === 'X') {
-                 computerSide = '0'
-                 board[lineNumber][cellNumber] = computerSide;
-             } else {
-                 computerSide = 'X';
-                 board[lineNumber][cellNumber] = computerSide;
-             }
+        lineNumber = Math.floor((Math.random() * (3)));
+        cellNumber = Math.floor((Math.random() * (3)));
+        if (!board[lineNumber][cellNumber]) {
+            //Update value in array
+            if (userSide === 'X') {
+                computerSide = '0'
+                board[lineNumber][cellNumber] = computerSide;
+            } else {
+                computerSide = 'X';
+                board[lineNumber][cellNumber] = computerSide;
+            }
             condition = false;
-             game.setBoard(board);
-         }
+            game.setBoard(board);
+        }
     }
 
     const computerSelectedCell = document.querySelector(`#cell-${lineNumber}${cellNumber}`);
-    if(userSide === 'X') {
+    if (userSide === 'X') {
         computerSelectedCell.style = 'background: url("../images/ellipse.png") no-repeat center';
 
     } else {
         computerSelectedCell.style = 'background: url("../images/cross.png") no-repeat center';
     }
-    if(game.checkWinner(computerSide)){
+    if (game.checkWinner(computerSide)) {
         endRound();
     }
 
@@ -230,8 +200,8 @@ function endRound(winner) {
     const winnerText = document.querySelector('.game-board_whose-won-round');
     nextRoundBtn.style = 'display: block';
     winnerText.textContent = `${winner} win the round!`
-    cells.forEach(cell => cell.removeEventListener('click',game.makeTurn));
-    nextRoundBtn.addEventListener('click',(e) => {
+    cells.forEach(cell => cell.removeEventListener('click', game.makeTurn));
+    nextRoundBtn.addEventListener('click', () => {
         winnerText.textContent = '';
         // playRound();
     });
